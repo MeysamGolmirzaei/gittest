5a7bc1d9cd4633dd54dc903013249da6343839fa
majidpashaei <dell3porde@gmail.com>
2023-10-28 18:45:17 +0330

    secondcomit test

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -2,7 +2,8 @@ namespace git.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
+    public string? rid { get; set; }
+    public string Name { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
